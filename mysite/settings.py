ceb19edfbd732bd6a3304cf34775f7e3e73c86c9
Jaywel Javier <jaywel@philsmile.com>
2020-12-21 13:07:26 +0800

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,5 +133,8 @@ PHONENUMBER_DEFAULT_REGION = 'PH'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
